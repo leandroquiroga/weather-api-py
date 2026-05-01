2f3f477d771c3ddd7765c45b03c7ae5a44ec3554
leandroquiroga <leandroquiroga9514@gmail.com>
2026-05-01 19:49:11 -0300

    add proyect setup and configuration files

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.routes.weather_router import weather_router
+
+app = FastAPI(
+    title="Weather API",
+    description="A simple API to fetch weather data for a given city.",
+    version="1.0.0",
+)
+
+
+app.include_router(weather_router)

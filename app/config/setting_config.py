a35e5e6fc228644aaaa85edac3a6b9dee2a07e85
leandroquiroga <leandroquiroga9514@gmail.com>
2026-05-01 21:10:06 -0300

    refactor implement Settings class for  environment management

--- app/config/setting_config.py
+++ app/config/setting_config.py
@@ -1,25 +1,23 @@
-import os
-from dotenv import load_dotenv
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-load_dotenv()  # Carga las variables de entorno desde el archivo .env
-
-
-def _require_env(name: str) -> str:
-    """ 
-    Funcion para cargar las variables de entorno necesarias para el proyecto.
-    Args:
-        name (str): Nombre de la variable de entorno a cargar.  
-    Returns: 
-        str: Valor de la variable de entorno cargada.
-    """
+class Settings(BaseSettings):
+    """Clase de configuracion"""
+    URL_BASE_WEATHER_API = str = Field(validation_alias="ACCESS_URL_BASE_WEATHER_API")
+    PATH_CITY_WEATHER = str = Field(validation_alias="ACCESS_PATH_CITY_WEATHER")
+    WEATHER_API_KEY = str = Field(validation_alias="ACCESS_WEATHER_API_KEY")
+    REDIS_HOST = str = Field(validation_alias="ACCESS_REDIS_HOST")
     
-    value = os.getenv(name)
-    if value is None:
-        raise ValueError(f"Variable de entorno '{name}' no encontrada.")
-    return value
+    REDIS_PORT = int = Field(validation_alias="ACCESS_REDIS_PORT")
+    REDIS_DB = int = Field(validation_alias="ACCESS_REDIS_DB")
+    CACHE_TTL_SECONDS = int = Field(validation_alias="ACCESS_CACHE_TTL_SECONDS")
+    
+    model_config = SettingsConfigDict(
+        env_file = ".env",
+        env_file_encoding = "utf-8"
+      
+    )
+    
+settings = Settings()
 
-WEATHER_API_KEY = _require_env("ACCESS_WEATHER_API_KEY")
-REDIS_HOST = _require_env("ACCESS_REDIS_HOST")
-REDIS_PORT = int(_require_env("ACCESS_REDIS_PORT"))
-REDIS_DB = int(_require_env("ACCESS_REDIS_DB"))
-CACHE_TTL_SECONDS = int(_require_env("ACCESS_CACHE_TTL_SECONDS"))
+

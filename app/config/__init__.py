085bbfdfd22dd945d24acf227cf1631138b39efe
leandroquiroga <leandroquiroga9514@gmail.com>
2026-05-04 19:27:39 -0300

    add Pydantic validation to Settings class

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,1 +1,1 @@
-from .setting_config import WEATHER_API_KEY, REDIS_HOST, REDIS_PORT, REDIS_DB, CACHE_TTL_SECONDS
+from .setting_config import settings
--- app/config/setting_config.py
+++ app/config/setting_config.py
@@ -3,14 +3,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     """Clase de configuracion"""
-    URL_BASE_WEATHER_API = str = Field(validation_alias="ACCESS_URL_BASE_WEATHER_API")
-    PATH_CITY_WEATHER = str = Field(validation_alias="ACCESS_PATH_CITY_WEATHER")
-    WEATHER_API_KEY = str = Field(validation_alias="ACCESS_WEATHER_API_KEY")
-    REDIS_HOST = str = Field(validation_alias="ACCESS_REDIS_HOST")
+    URL_BASE_WEATHER_API: str = Field(validation_alias="ACCESS_URL_BASE_WEATHER_API")
+    PATH_CITY_WEATHER: str = Field(validation_alias="ACCESS_PATH_CITY_WEATHER")
+    WEATHER_API_KEY: str = Field(validation_alias="ACCESS_WEATHER_API_KEY")
+    REDIS_HOST: str = Field(validation_alias="ACCESS_REDIS_HOST")
     
-    REDIS_PORT = int = Field(validation_alias="ACCESS_REDIS_PORT")
-    REDIS_DB = int = Field(validation_alias="ACCESS_REDIS_DB")
-    CACHE_TTL_SECONDS = int = Field(validation_alias="ACCESS_CACHE_TTL_SECONDS")
+    REDIS_PORT: int = Field(validation_alias="ACCESS_REDIS_PORT")
+    REDIS_DB: int = Field(validation_alias="ACCESS_REDIS_DB")
+    CACHE_TTL_SECONDS: int = Field(validation_alias="ACCESS_CACHE_TTL_SECONDS")
     
     model_config = SettingsConfigDict(
         env_file = ".env",

b5a6ff39d42fc3c0ebd94ff8ce46a71200b13860
leandroquiroga <leandroquiroga9514@gmail.com>
2026-05-09 15:00:04 -0300

    implement singleton pattern for Redis client and lifespan

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
-from app.utils.errors.expetions_handlers import (
+from contextlib import asynccontextmanager
+
+from app.utils.errors.exceptions_handlers import (
     handle_city_not_found,
     handle_external_api_exception,
     handle_cache_exception,
@@ -18,11 +20,27 @@ from app.utils.errors.custom_exceptions import (
 
 from app.routes.weather_router import weather_router
 from app.middlewares.rate_limit_middleware import limiter
+from app.clients.redis_client import get_redis_client
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    redis_client = await get_redis_client()
+    app.state.redis = redis_client
+    yield
+    # Shutdown
+    await app.state.redis.close()
+    
+
+
 app = FastAPI(
     title="Weather API",
     description="A simple API to fetch weather data for a given city.",
     version="1.0.0",
+    lifespan=lifespan
 )
+
+
 
 app.state.limiter = limiter
 

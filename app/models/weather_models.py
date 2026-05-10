d6492270ca5ae77818015a0876503f7c021e2d34
leandroquiroga <leandroquiroga9514@gmail.com>
2026-05-10 17:48:11 -0300

    update WeatherData DTO for optimized data transfer

--- app/models/weather_models.py
+++ app/models/weather_models.py
@@ -1,16 +1,56 @@
 from pydantic import BaseModel
 
+class Coord(BaseModel):
+    """DTO for coordinates"""
+    lon: float
+    lat: float
+    
+class WeatherCondition(BaseModel):
+    """ DTO for weather condition"""
+    id: int
+    main: str
+    description: str
+    icon: str
+    
+class MainWeatherData(BaseModel):
+    """ DTO for main weather data"""
+    temp: float
+    feels_like: float
+    temp_min: float
+    temp_max: float
+    pressure: int
+    humidity: int
+    sea_level: int | None = None
+    grnd_level: int | None = None
+    
+class Wind(BaseModel):
+    """ DTO for wind data"""
+    speed: float
+    deg: int
+    gust: float | None = None
+    
+class Clouds(BaseModel):
+    """ DTO for cloud data"""
+    all: int
+    
+class Sys(BaseModel):
+    """ DTO for system data"""
+    type: int
+    id: int
+    country: str
+    sunrise: int
+    sunset: int
 class WeatherData(BaseModel):
     """DTO for weather data"""
-    coord: dict[str, float]
-    weather: list[dict[str, int | str]]
+    coord: Coord
+    weather: list[WeatherCondition]
     base: str
-    main: dict[str, float | int]
+    main: MainWeatherData
     visibility: int
-    wind: dict[str, float | int]
-    clouds: dict[str, int]
+    wind: Wind
+    clouds: Clouds
     dt: int
-    sys: dict[str, int | str]
+    sys: Sys
     id: int
     name: str
     cod: int
